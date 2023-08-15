4d56ee8f25d8b710ae834a9099feb4c9489ae6ee
roberto16477 <37126197+roberto16477@users.noreply.github.com>
2023-08-15 14:21:53 +0000

    observacoes

---
+++ exlista1.py
@@ -0,0 +1,7 @@
+cont = 0
+L = []
+while cont != 10:
+    valor = int(input("Informe um valor: "))
+    L[cont] = valor
+
+print(L)
---
+++ testelista.py
@@ -0,0 +1,8 @@
+L = [10, 6, 8, 20]
+
+print (len(L)) # função len conta os elementos da lista
+L.append(69) #função append adiciona um valor no final da lista
+print(L)
+
+del L[0] #remove o item selecionado pelo indice da lista
+print(L)
