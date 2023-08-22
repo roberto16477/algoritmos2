c812203497ceff3755ec4dec330fc9f1f5fc8acc
roberto16477 <37126197+roberto16477@users.noreply.github.com>
2023-08-22 14:36:24 +0000

    completo

--- ex22.08/ex1.py
+++ ex22.08/ex1.py
@@ -16,3 +16,9 @@ print(v[x*3])
 
 print("Mais elaborados:")
 print(v[v[x+y]]) # está pegando a posição 1 do vetor pois o valor na posição 6 é 1
+print(v[x+y])    # item na posição 6
+print(v[8-v[2]]) # ele encontra o v2 e subtrai do valor 8
+# print(v[v[4]]) # da erro pois o indice de posição 4 tem o valor 10, e não existe indice 10 para buscar depois
+print(v[ v[ v[0]]]) # o exercicio pediu indice 7 pra dar erro como no anterior
+# print(v[v[1]*v[4]]) #erro de estouro também
+print(v[x+4])
