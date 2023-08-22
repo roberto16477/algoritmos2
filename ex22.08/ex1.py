2d002f998788ebc7d47c60cf67cc550aad6c5421
roberto16477 <37126197+roberto16477@users.noreply.github.com>
2023-08-22 13:58:34 +0000

    first ex

--- ex22.08/ex1.py
+++ ex22.08/ex1.py
@@ -1,1 +1,15 @@
-print ("Só pra commit")
+v = [2, 6, 8, 3, 10, 9, 1, 21, 33, 14]
+x = 2
+y = 4
+
+print("Somados: ")
+print(v[x+1])
+print(v[x+2])
+print(v[x+3])
+
+print("Multiplicados: ")
+
+print(v[x*4])
+print(v[x*1])
+print(v[x*2])
+print(v[x*3])
