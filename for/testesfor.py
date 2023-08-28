c4f52063e8168471efcc37b403ccb6ed1f7a6c14
roberto16477 <37126197+roberto16477@users.noreply.github.com>
2023-08-28 19:35:30 +0000

    up do testes for

---
+++ for/testesfor.py
@@ -0,0 +1,15 @@
+"""
+for num in range(11):
+    print(num)          #forma padrão de uso de range, o primeiro é 0 por padrão e para 1 antes
+
+for num in range(3, 11): # define o primeiro indice como 3 e finaliza no 10
+    print(num)
+
+for num in range(0, 11, 2):  #o terceiro valor determina o incremento (ou passo)
+    print(num)
+
+
+"""
+for num in range(10, 0, -1):    #começa em 10, ultimo é 0 (para em 1) e anda de 1 em 1 (negativo)
+    print(num)
+
