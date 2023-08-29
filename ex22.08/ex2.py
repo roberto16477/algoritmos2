04b9d5712bf55f539cdba1ad2c3a140eff4be113
roberto16477 <37126197+roberto16477@users.noreply.github.com>
2023-08-29 11:55:17 +0000

    att

--- ex22.08/ex2.py
+++ ex22.08/ex2.py
@@ -1,7 +1,17 @@
+n = 9
 v = []
+v2 = []
 
-for num in range(5):
+for num in range(9):
     valor = int(input("Digite um número: "))
     v.append(valor)
 
-print(v)
+for i in range(n):
+    mult = 0
+    for j in range (2,v[i]-1):
+        if v[i] % j == 0:
+            mult += 1
+    if mult == 0:
+        v2.append(i)
+
+print(v2)
