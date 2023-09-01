c484396d14d2f019c6ea8b78cb214ed59d29aa64
roberto16477 <37126197+roberto16477@users.noreply.github.com>
2023-09-01 18:56:36 +0000

    att

--- ex22.08/arrays/exemplo1.py
+++ ex22.08/arrays/exemplo1.py
@@ -7,6 +7,7 @@ for i in range(n):
     vetor[i] = int(input("Informe um valor: "))
 
 for i in range(n):
-    print(f'V[{i}] = {vetor[i]}')
+   # print(f'V[{i}] = {vetor[i]}')
+    print("Posição [", i,"] = ", vetor[i])
 
 #o ultimo for exibe os valores do vetor e seu índice
