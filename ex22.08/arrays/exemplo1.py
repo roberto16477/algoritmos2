f75c0027fcecda49d19a47aef5dd7d62ca46d910
roberto16477 <37126197+roberto16477@users.noreply.github.com>
2023-09-01 18:24:22 +0000

    att exemplo

--- ex22.08/arrays/exemplo1.py
+++ ex22.08/arrays/exemplo1.py
@@ -6,4 +6,7 @@ vetor = np.zeros(n)
 for i in range(n):
     vetor[i] = int(input("Informe um valor: "))
 
-print(vetor)
+for i in range(n):
+    print(f'V[{i}] = {vetor[i]}')
+
+#o ultimo for exibe os valores do vetor e seu índice
