f49b75be56b8bb30f912394663fdf532b2a23037
roberto16477 <37126197+roberto16477@users.noreply.github.com>
2023-09-05 18:42:10 +0000

    exemplo inicial de listas

---
+++ listas/exemplo1.py
@@ -0,0 +1,17 @@
+notas = [0, 0, 0, 0, 0]
+soma = 0
+x = 0
+
+while x < 5:
+    notas[x] = float(input(f"Informe a nota {x} : "))
+    soma = soma + notas[x]
+    x = x+1
+
+x = 0
+
+while x < 5:
+    print(f"nota {x} : {notas[x]:6.2f}")
+    x+=1
+print(f"media: {soma/ x:5.2f}")
+
+print(type(notas8))
