993f4cf5bdd2a192c14df397e785f0a2f54aae0f
roberto16477 <37126197+roberto16477@users.noreply.github.com>
2023-09-25 19:00:43 +0000

    exibindo cada elemento de cada item de uma matriz

---
+++ Matrizes/exemplo5.py
@@ -0,0 +1,10 @@
+matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+print("Matriz 3x3: ", matriz)
+
+print("Matriz impressa de outra forma:")
+for item in matriz:
+    for elemento in item:
+        print(elemento, end=' ')
+    print()
+
