b134fb010c38c3e7e5a6f21a1fe4b66ea450d51c
roberto16477 <37126197+roberto16477@users.noreply.github.com>
2023-09-25 23:08:36 +0000

    ex 2

---
+++ Matrizes/lista2/exercicio2.py
@@ -0,0 +1,29 @@
+M = []
+N = 5
+
+
+for l in range(N):
+    lista = []
+    for c in range(N):
+        lista.append(int(input("Informe um valor para a matriz: ")))
+    M.append(lista)
+
+
+print("Matriz Original:")
+for num in M:
+        print(num)
+
+for i in range(N):
+    auxl = M[2][i]
+    M[2][i] = M[4][i]
+    M[4][i] = auxl
+
+for i in range(N):
+    aux = M[1][i]
+    M[1][i] = M[i][4]
+    M[i][4] = aux
+
+print("Alterada: ")
+
+for valor in M:
+     print(valor)
