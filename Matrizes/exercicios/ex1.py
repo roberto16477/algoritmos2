810f1cfab4534757ca86c8b3693c971fad13c331
roberto16477 <37126197+roberto16477@users.noreply.github.com>
2023-09-25 19:40:36 +0000

    ex 1

--- Matrizes/exercicios/ex1.py
+++ Matrizes/exercicios/ex1.py
@@ -1,10 +1,10 @@
 M = []
-N = 3
+N = 5
 
 for i in range(N):
     lista = []
     for j in range(N):
         lista.append(int(input("Informe um valor: ")))
-        M.append(lista)
+    M.append(lista)
 
 print(M)
