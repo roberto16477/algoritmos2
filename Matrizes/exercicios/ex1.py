dae07cf7c9735bc53b4dec93b381e775a632fce4
roberto16477 <37126197+roberto16477@users.noreply.github.com>
2023-09-25 19:31:48 +0000

    ex 1

--- Matrizes/exercicios/ex1.py
+++ Matrizes/exercicios/ex1.py
@@ -4,7 +4,7 @@ N = 3
 for i in range(N):
     lista = []
     for j in range(N):
-        lista.append(input("Informe um valor para adicionar a matriz: "))
+        lista.append(int(input("Informe um valor: ")))
         M.append(lista)
 
 print(M)
