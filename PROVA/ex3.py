7b471c6d6ec375c192e21a874d778a60533b39aa
roberto16477 <37126197+roberto16477@users.noreply.github.com>
2023-09-26 12:40:38 +0000

    ex3 - par e impar

---
+++ PROVA/ex3.py
@@ -0,0 +1,21 @@
+X = []
+N = 10
+cont = 0
+contimp = 0
+soma = 0
+media = 0
+
+for i in range(N):
+    X.append(int(input("Informe um valor: ")))
+
+for j in range(N):
+    if X[j] % 2 == 0:
+        cont = cont + 1
+    else:
+        soma = soma + X[j]
+        contimp += 1
+    media = soma/contimp
+
+
+print("O total de numeros pares em X é: ", cont)
+print("A média dos valorem impares em X é: ", media)
