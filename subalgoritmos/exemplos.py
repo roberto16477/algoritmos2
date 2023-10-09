1f65623df7d540c900e9cca01f57bcde94da1e18
roberto16477 <37126197+roberto16477@users.noreply.github.com>
2023-10-09 19:55:09 +0000

    exemplo 1 - procedimento e função

---
+++ subalgoritmos/exemplos.py
@@ -0,0 +1,9 @@
+def soma(a, b):
+    print(a+b)    #procedimento
+
+soma(1, 2)    #chama desse jeito porque o print está dentro do procedimento
+
+def somaf(a, b):
+    return(a+b)    #função
+
+print(somaf(1, 3))      #chama assim pois a função apenas faz o processamento, para printar temos que chamar em um print
