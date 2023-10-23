1e5c81a4fa46b8931a5ed6637c1b9d8f41e521f6
roberto16477 <37126197+roberto16477@users.noreply.github.com>
2023-10-23 20:45:45 +0000

    ex3 - primo em intervalo

--- matematica/exercicio3.py
+++ matematica/exercicio3.py
@@ -1,15 +1,16 @@
 INICIO = int(input("Informe o primeiro número do intervalo: "))
 FINAL = int(input("Informe o último número do intervalo: "))
 V = []
-mult = 0
+
 
 for j in range (INICIO, FINAL):
+    mult = 0
     for i in range(2, j):
         if j % i == 0:
-            mult += 1
-            V.append(j)
+            mult +=1
+            break
+    if mult == 0:
+        V.append(j)
 
-if mult == 0:
-    print(N, "é um número primo!")
-else:
-    print(N, "é divisivel por: ", V)
+print("Os primos entre ", INICIO, " e ", FINAL, "são:")
+print(V)
