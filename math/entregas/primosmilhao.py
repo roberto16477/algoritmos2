4131756640ad762c2c14d424c08b4b54440058d2
roberto16477 <37126197+roberto16477@users.noreply.github.com>
2023-10-30 21:22:32 +0000

    primos milhao

--- math/entregas/primosmilhao.py
+++ math/entregas/primosmilhao.py
@@ -2,8 +2,9 @@ INICIO = 2
 FINAL = 100
 V = []
 primo = False
+primosmilhao = []
 
-for j in range (INICIO, FINAL):
+for j in range (INICIO, FINAL+1):
     mult = 0
     for i in range(2, j):
         if j % i == 0:
@@ -15,10 +16,22 @@ for j in range (INICIO, FINAL):
 inicio2 = int(input("Informe o inicio do intervalo: "))
 final2 = int(input("Informe o final do intervalo: "))
 
-for k in range(inicio2, final2):
+for k in range(inicio2, final2+1):
+    primo = False
     for l in V:
         if k % l == 0:
+            primo = False
             break
         else:
-            
+            primo = True
+    if primo == True:
+        primosmilhao.append(k)
+    
+
+print(V)
+print(primosmilhao)
+
+
+        
+
 
