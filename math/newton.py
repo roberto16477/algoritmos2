00ae9a52e5cb0129ba364aeb2fd641c09ddcf101
roberto16477 <37126197+roberto16477@users.noreply.github.com>
2023-11-09 13:46:41 +0000

    calculo raiz com newton

--- math/newton.py
+++ math/newton.py
@@ -1,17 +1,17 @@
-import time
+#import time
 
 def calcular_raiz_quadrada(numero, precisao=1e-6):
     if numero < 0:
         return None  # Lidando com números negativos (raiz quadrada não real)
 
     aproximacao = numero  # Comece com uma estimativa inicial
-    while abs(aproximacao * aproximacao - numero) > precisao:
+    while abs(aproximacao * aproximacao - numero) > precisao:   # o abs impede a duplicação dos números positivos e negativos
         aproximacao = 0.5 * (aproximacao + numero / aproximacao)
 
     return aproximacao
 
 
-num = float(input("Insira um número para calcular a raiz quadrada: "))  # Número para calcular a raiz quadrada
+num = float(input("Insira um número para calcular a raiz quadrada: "))
 raiz_quadrada = calcular_raiz_quadrada(num)
 print(f"A raiz quadrada de {num} é aproximadamente {raiz_quadrada:.6f}")
 
