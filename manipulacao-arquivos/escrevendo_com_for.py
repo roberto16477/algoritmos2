63d4be0e4c1850664ddfd98592d30b3af6c3c210
roberto16477 <37126197+roberto16477@users.noreply.github.com>
2023-11-09 14:20:31 +0000

    varias linhas com for

--- manipulacao-arquivos/escrevendo_com_for.py
+++ manipulacao-arquivos/escrevendo_com_for.py
@@ -1,6 +1,6 @@
 arquivo = open("manipulacao-arquivos/textos/variaslinhas.txt", "w", encoding="utf-8")
 
 for linha in range(1, 101):
-    arquivo.write(linha)
+    arquivo.write(f"{linha}\n")
 
 arquivo.close()
