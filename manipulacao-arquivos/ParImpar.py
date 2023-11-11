e5f4f56564d7aa5089330e0fa836c586153149bd
roberto16477 <37126197+roberto16477@users.noreply.github.com>
2023-11-11 22:29:29 +0000

    gerando arquivos com with

--- manipulacao-arquivos/ParImpar.py
+++ manipulacao-arquivos/ParImpar.py
@@ -1,6 +1,6 @@
 #gerando arquivos com resultados que obtiver com o código
-with open ("manipulacao-arquivos/textos/impares.txt", "w") as impares:
-    with open("manipulacao-arquivos/textos/pares.txt", "w") as pares:
+with open ("manipulacao-arquivos/textos/impares.txt", "w", encoding="utf-8") as impares:
+    with open("manipulacao-arquivos/textos/pares.txt", "w", encoding="utf-8") as pares:
         for i in range(0, 1000):
             if i % 2 == 0:
                 pares.write(f"{i}\n")
