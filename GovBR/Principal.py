d689d638c9793a7df895bc43293b2a7ad244dfe3
roberto16477 <37126197+roberto16477@users.noreply.github.com>
2023-11-13 22:07:40 +0000

    iniciando a parte principal do código

--- GovBR/Principal.py
+++ GovBR/Principal.py
@@ -1,9 +1,5 @@
-with open("GovBR/dominios.csv","r", encoding="utf-8") as dominios:
-    todos = dominios.readlines()
+from funcoes import *
 
-informacao = input("Informe uma palavra para verificar seus domínios: ")
+busca = input("Informe uma palavra para verificar seus domínios: ")
 
-for linha in todos:
-    colunas = linha.split('|')
-    if informacao in linha:
-        print(colunas[1])
+AgrupaPorPalavra(busca)
