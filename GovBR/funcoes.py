1de7982417a341ac483924e0747290e3cdf3afd2
roberto16477 <37126197+roberto16477@users.noreply.github.com>
2023-11-13 23:19:24 +0000

    Up inicial da função ConsultaDominio

--- GovBR/funcoes.py
+++ GovBR/funcoes.py
@@ -8,3 +8,13 @@ def AgrupaPorPalavra(palavra):
         colunas = linha.split('|')
         if informacao in linha:
             print(colunas[1])
+
+def ConsultaDominio(dominio):
+    with open("GovBR/dominios.csv","r", encoding="utf-8") as dominios:
+        todos = dominios.readlines()
+
+        busca = (dominio + ".gov.br")
+
+        for linha in todos:
+            if busca in linha:
+                print(linha)
