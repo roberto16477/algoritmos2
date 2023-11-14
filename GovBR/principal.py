1b754ef39aca968202a2825c2629cc489bfa999d
roberto16477 <37126197+roberto16477@users.noreply.github.com>
2023-11-14 12:46:19 +0000

    Up inicial do menu

--- GovBR/principal.py
+++ GovBR/principal.py
@@ -1,5 +1,31 @@
 from funcoes import *
 
-busca = input("Informe uma palavra para verificar seus domínios: ")
+opcao = 30
+cont = 0
+repetir = 0
 
-ConsultaDominio(busca)
+while opcao != 0:
+    if cont >= 1:
+        print("Informe 1 para SIM")
+        print("Informe 2 para NÃO")
+        repetir = int(input("Deseja repetir a operação?"))
+        
+    if repetir != 1:
+        print("Informe 0 para sair")
+        print("Informe 3 para procurar por domínios relacionados a uma palavra específica")
+        print("Informe 5 para consultar a disponibilidade de um domínio")
+        opcao = int(input("Informe o número da operação que deseja: "))
+
+    if opcao == 3:
+        busca = input("Informe uma palavra para verificar seus domínios: ")
+
+        AgrupaPorPalavra(busca)
+
+    if opcao == 5:
+        busca = input("Informe um domínio que deseja consultar do .gov.br")
+
+        ConsultaDominio(busca)
+
+    cont += 1
+
+print("Fim da execução")
