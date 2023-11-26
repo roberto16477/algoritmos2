41622280220f9026d4af3d04e6b5ec32accedc2f
roberto16477 <37126197+roberto16477@users.noreply.github.com>
2023-11-26 21:00:07 +0000

    Up final do trabalho de matematica

---
+++ math/trabalho.py
@@ -0,0 +1,38 @@
+numinicial = int(input("Informe o inicio do intervalo: "))
+numfinal = int(input("Informe o final do intervalo: "))
+Divisores = []
+primosintervalo = []
+V = []
+primo = False
+cont = 0
+
+raiz = numfinal**0.5
+
+RaizInteira = int(raiz)
+
+for j in range (2, RaizInteira+1):
+    mult = 0
+    for i in range(2, j):
+        if j % i == 0:
+            mult +=1
+            break
+    if mult == 0:
+        V.append(j)
+
+print("Os primos até a raiz inteira é: ", V)
+
+for k in range(numinicial, numfinal+1):
+    primo = False
+    for l in V:
+        if k % l == 0:
+            primo = False
+            break
+        else:
+            primo = True
+    if primo == True:
+        primosintervalo.append(k)
+        cont += 1
+    
+
+print("Os primos no intervalo informado são:", primosintervalo)
+print("Existem", cont, "números primos no intervalo selecionado!")
