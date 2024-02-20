3db60897d5145b12a49f26ef562b22e45c6ad9d8
Roberto Carlos da Silva Junior <37126197+roberto16477@users.noreply.github.com>
2024-02-20 13:50:22 +0000

    up

--- Revisao/ex1.py
+++ Revisao/ex1.py
@@ -1,1 +1,12 @@
-print("up")
+n = 5
+V1 = []
+V2 = []
+
+for i in range(n):
+    V1.append(float(input("Informe um valor para o primeiro vetor: ")))
+
+for j in range(n):
+    V2.append(float(input("Informe um número para o segundo vetor: ")))
+
+print(V1)
+print(V2)
