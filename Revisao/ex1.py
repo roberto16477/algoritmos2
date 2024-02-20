302d1ce24ea47a890604416df305a5eef8cfc505
Roberto Carlos da Silva Junior <37126197+roberto16477@users.noreply.github.com>
2024-02-20 14:00:33 +0000

    up

--- Revisao/ex1.py
+++ Revisao/ex1.py
@@ -2,11 +2,20 @@ n = 5
 V1 = []
 V2 = []
 
+V3 = []
+produtoescalar = 0
+
 for i in range(n):
     V1.append(float(input("Informe um valor para o primeiro vetor: ")))
 
 for j in range(n):
     V2.append(float(input("Informe um número para o segundo vetor: ")))
 
-print(V1)
-print(V2)
+for k in range(n):
+    V3.append(V1[k]*V2[k])
+
+produtoescalar = sum(V3)
+
+print("O vetor 1 é: ", V1)
+print("O vetor 2 é: ",V2)
+print("O produto escalar é: ", produtoescalar)
