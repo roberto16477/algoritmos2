4d3d423e7583c7504134bf2b2d807370f93c755f
Roberto Carlos da Silva Junior <37126197+roberto16477@users.noreply.github.com>
2024-02-25 20:07:20 +0000

    matriz 10x10 com regras doidas

---
+++ Revisao/ex5.py
@@ -0,0 +1,15 @@
+M = []
+N = 10
+
+for i in range(N):
+    lista = []
+    for j in range(N):
+        if i == j:
+            lista.append(3*i**2)
+        elif i < j:
+            lista.append((2*i)+(7*j))
+        else:
+            lista.append((4*i**3)+(5*j**2)+(1))
+    M.append(lista)
+
+print(M)
