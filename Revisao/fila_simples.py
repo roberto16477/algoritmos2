1543cc32bf8ed68edfb25d617e7a3222c0345993
Roberto Carlos da Silva Junior <37126197+roberto16477@users.noreply.github.com>
2024-04-23 14:16:29 +0000

    up simples

--- Revisao/fila_simples.py
+++ Revisao/fila_simples.py
@@ -1,1 +1,12 @@
-print("oi")
+def inicia(tam, arr):
+    for i in range(tam):
+        arr.append('')
+
+def insere(tam, arr):
+    val = input("Informe um valor para inserir: ")
+    for i in range(tam):
+        if arr[i] == '':
+            arr[i] = val
+        
+fila = []
+n = 5
