b50684a78ca0024bbab32bca7a0a905bddf4e4b4
Roberto Carlos da Silva Junior <37126197+roberto16477@users.noreply.github.com>
2024-04-25 20:34:24 +0000

    up fila simples

--- Revisao/fila_simples.py
+++ Revisao/fila_simples.py
@@ -7,6 +7,39 @@ def insere(tam, arr):
     for i in range(tam):
         if arr[i] == '':
             arr[i] = val
-        
+            break
+
+'''def insere(l):
+    val = input("Informe o valor de deseja inserir: ")
+    l.append(val)'''
+
+'''def tira(l):
+    if len(l) > 0:
+        l.pop(0)
+    else:
+        print("A fila já está vazia!")'''
+
+def tira(tam, arr):
+    for i in range(tam):
+        if arr[i] != '':
+            arr[i] = ''
+            break
+
 fila = []
 n = 5
+inicia(n, fila)
+opcao = -1
+
+while opcao != 0:
+    print("Informe 0 para sair")
+    print("Informe 1 para inserir na fila")
+    print("Informe 2 para remover um elemento da fila")
+    opcao = int(input("Informe a opção que deseja: "))
+
+    if opcao == 1:
+        insere(n, fila)
+        print(f"\n {fila}")
+
+    if opcao == 2:
+        tira(n, fila)
+        print(fila)
